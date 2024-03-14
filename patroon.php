8b448169e91291e3f7b9772e30c1d2ed6a04e547
sim--on97 <970229452@st.deltion.nl>
2024-03-14 09:45:09 +0100

    een_halfe_kerstboom

---
+++ patroon.php
@@ -0,0 +1,13 @@
+<?php
+
+
+//een patroon tekenen op met de cli
+
+//$sterretjes = "*\n **\n ***\n ****\n *****\n";
+//$half = "halfe kerstboom";
+
+function patroon (){
+    echo"*\n**\n***\n****\n*****\n";
+}
+patroon();
+?>
